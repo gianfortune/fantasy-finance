13cce25d05afac983a4adeba1aa47fa3422998c8
Marisa Gianfortune <mng35@cornell.edu>
2017-06-11 22:19:59 -0400

    Updates README.md and changes the binding of the component to take in the parent's data structure.

--- js/financeController.js
+++ js/financeController.js
@@ -1,10 +1,40 @@
 angular.module('financeApp', ['chart.js'])
   .controller('financeController', function($interval) {
     var parent = this;
+    parent.dataCurrencies = [{
+      fantasyMoney: 'Galactic Standard Credits',
+      fantasyType: "Star Wars",
+      data: [],
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      series: ['GSC', 'USD']
+    }, {
+      fantasyMoney: 'Munny',
+      fantasyType: "Kingdom Hearts",
+      data: [],
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September"],
+      series: ['MU', 'USD']
+    }]
 
-    parent.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+    parent.$onInit = function() {
+       parent.fetchData();
+     };
+
+     parent.fetchData = function () {
+       parent.dataCurrencies.forEach(function (currencyInfo) {
+         for(var i = 0; i < currencyInfo.series.length; i++) {
+           var newData = parent.makeRandom100(currencyInfo.labels.length);
+           currencyInfo.data.push(newData)
+         }
+       })
+     }
+
+     parent.makeRandom100 = function (length) {
+       let dataArray = [];
+       for(var i = 0; i < length; i++) {
+         let newData = Math.floor((Math.random() * 100) + 1);
+         dataArray.push(newData)
+       }
+       return dataArray
+     }
 
   });
--- js/moneychartcontroller.js
+++ js/moneychartcontroller.js
@@ -1,8 +1,6 @@
 function moneyChartController($interval) {
   var ctrl = this;
-
-  ctrl.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  ctrl.series = ['Munny', 'USD'];
+  
   ctrl.height_chart = window.innerHeight*0.7;
   ctrl.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
   ctrl.options = {
@@ -29,8 +27,7 @@ function moneyChartController($interval) {
   $interval( function(){ ctrl.updateGraphData(); }, 2000)
 
   ctrl.updateGraphData = function () {
-    console.log(ctrl)
-    ctrl.data.forEach(function(row){
+    ctrl.currencyObject.data.forEach(function(row){
       var newData = Math.floor((Math.random() * 100) + 1)
       row.push(newData)
       row.shift()
@@ -43,6 +40,6 @@ angular.module('financeApp').component('moneyChart', {
   templateUrl: 'html/moneyChart.html',
   controller: moneyChartController,
   bindings: {
-    data: '<'
+    currencyObject: '<'
   }
 });
